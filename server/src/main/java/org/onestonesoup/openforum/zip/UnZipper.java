e9bd9c95c05a4e072b86923c3b9c12f4fdfb8efe
nik <nikcross@wet-wired.com>
2015-09-25 09:46:45 +0100

    Fixed zip and unzip

--- server/src/main/java/org/onestonesoup/openforum/zip/UnZipper.java
+++ server/src/main/java/org/onestonesoup/openforum/zip/UnZipper.java
@@ -9,55 +9,48 @@ import org.onestonesoup.openforum.filemanager.ResourceFolder;
 import org.onestonesoup.openforum.filemanager.ResourceStore;
 
 public class UnZipper {
-	
+
 	private ZipInputStream zipInputStream;
-	private String[] excludeList=null;
-	
-	public UnZipper(InputStream iStream) throws IOException
-	{
-		zipInputStream = new ZipInputStream( iStream );
+	private String[] excludeList = null;
+
+	public UnZipper(InputStream iStream) throws IOException {
+		zipInputStream = new ZipInputStream(iStream);
 	}
-	
-	public void unzipAll(ResourceStore resourceStore,ResourceFolder targetFolder) throws IOException
-	{
+
+	public void unzipAll(ResourceStore resourceStore,
+			ResourceFolder targetFolder) throws IOException {
 		ZipEntry entry = zipInputStream.getNextEntry();
-		
-		while(entry!=null)
-		{
-			if(isExcluded(entry.getName()))
-			{
+
+		while (entry != null) {
+			if (isExcluded(entry.getName())) {
 				entry = zipInputStream.getNextEntry();
 				continue;
 			}
-			
-			if(	entry.isDirectory() )
-			{
-				resourceStore.getResourceFolder(targetFolder+"/"+entry.getName(),true);
-			}
-			else
-			{
-				resourceStore.buildResource( resourceStore.getResourceFolder(targetFolder.getPath(),true) , entry.getName(), zipInputStream,entry.getSize());
+
+			if (entry.isDirectory()) {
+				resourceStore.getResourceFolder(
+						targetFolder + "/" + entry.getName(), true);
+			} else {
+				resourceStore.buildResource(resourceStore.getResourceFolder(
+						targetFolder.getPath(), true), entry.getName(),
+						zipInputStream, entry.getSize(),
+						false);
 			}
 			entry = zipInputStream.getNextEntry();
 		}
 	}
-	
-	public void setExcludeList(String[] excludeList)
-	{
+
+	public void setExcludeList(String[] excludeList) {
 		this.excludeList = excludeList;
 	}
-	
-	private boolean isExcluded(String entryName)
-	{
-		if(excludeList==null)
-		{
+
+	private boolean isExcluded(String entryName) {
+		if (excludeList == null) {
 			return false;
 		}
-		
-		for(int loop=0;loop<excludeList.length;loop++)
-		{
-			if(entryName.indexOf(excludeList[loop])==0)
-			{
+
+		for (int loop = 0; loop < excludeList.length; loop++) {
+			if (entryName.indexOf(excludeList[loop]) == 0) {
 				return true;
 			}
 		}
