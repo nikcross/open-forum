0fb2a39b9e775007148ec3147cabd631e4e15e3d
nik <nikcross@wet-wired.com>
2015-10-02 18:33:25 +0100

    Change to support monitoring store

--- server/src/main/java/org/onestonesoup/openforum/store/Store.java
+++ server/src/main/java/org/onestonesoup/openforum/store/Store.java
@@ -2,9 +2,14 @@ package org.onestonesoup.openforum.store;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Store {
 	private Map<String,Object> store = new HashMap<String,Object>();
+	
+	public Set<String> getKeys() {
+		return store.keySet();
+	}
 	
 	public Object get(String key) {
 		return store.get(key);
