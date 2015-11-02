4e7908ea190657cedba6b95c67fe19a7355b2837
nik <nikcross@wet-wired.com>
2015-11-02 19:22:01 +0000

    Changes to pages to purge

--- content/purge-changes.sh
+++ content/purge-changes.sh
@@ -1,6 +1,6 @@
 find ./changes/ -name history -type d -exec rm -rf {} \;
 rm -rf ./changes/OpenForum/Sandbox
-rm -rf ./changes/OpenForum/WikiJournal
+rm -rf ./changes/OpenForum/Journal
 rm -rf ./changes/OpenForum/MissingPages
 rm -rf ./changes/OpenForum/Deleted
 rm -rf ./changes/OpenForum/Temporary
--- content/purge-source.sh
+++ content/purge-source.sh
@@ -1,6 +1,6 @@
 find ./source/ -name history -type d -exec rm -rf {} \;
 rm -rf ./source/OpenForum/Sandbox
-rm -rf ./source/OpenForum/WikiJournal
+rm -rf ./source/OpenForum/Journal
 rm -rf ./source/OpenForum/MissingPages
 rm -rf ./source/OpenForum/Deleted
 rm -rf ./source/OpenForum/Temporary
