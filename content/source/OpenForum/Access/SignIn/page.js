aa7697ba5d03a851bf07653fe668d88332a0d36d
nik <nikcross@wet-wired.com>
2015-11-08 17:45:17 +0000

    Improved sign in page

--- content/source/OpenForum/Access/SignIn/page.js
+++ content/source/OpenForum/Access/SignIn/page.js
@@ -4,9 +4,9 @@ var result = " ";
 function signIn() { 
   
   var hash = hex_md5(""+Math.random());
-  var hashedPassword = hex_md5(password+hash);
+  var hashedPassword = hex_md5(passwordField+hash);
       
-  parameters = "userId="+userName+"&password="+hashedPassword+"&hash="+hash+"&flavour=json";
+  parameters = "userId="+userNameField+"&password="+hashedPassword+"&hash="+hash+"&flavour=json";
   
   JSON.post("/OpenForum/Access/SignIn/Process","signIn",parameters)
     .onSuccess(signedIn)
@@ -16,13 +16,20 @@ function signIn() {
 
 function signedIn(response) {
   if(response.result==="ok") {
-    currentUser = userName;
-    result = "<div data-alert class=\"alert-box success round\">Signed in as "+userName+"</div>";
+    currentUser = userNameField;
+    var nextPage = OpenForum.getParameter("forwardTo");
+    if(nextPage.length>0) {
+      result = "<div data-alert class=\"alert-box success round\">Signed in as "+userNameField+". Forwarding to "+nextPage+" soon...</div>";
+    } else {
+      result = "<div data-alert class=\"alert-box success round\">Signed in as "+userNameField+".</div>";
+    }
+    
+    setTimeout("window.location='"+nextPage+"'",3000);
   } else {
     result = "<div data-alert class=\"alert-box alert round\">"+response.errors+"</div>";
   }
 }
 
 function showError(response) {
-  
+    result = "<div data-alert class=\"alert-box alert round\">We have experieced a problem processing your sign in. "+response+"</div>";
 }
