d19a7619a594adb8fb5072125619bf2aaf8b72d8
nik <nikcross@wet-wired.com>
2015-11-08 17:56:17 +0000

    Removed old shell script

--- docker/docker-publish.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-echo "-> docker commit -m $3 -a Nik Cross "$2" nikcross/openforum:$1"
-docker commit -m "$3" -a "Nik Cross" "$2" "nikcross/openforum:$1"
-echo "-> docker push "nikcross/openforum:$1""
-docker push "nikcross/openforum:$1"
